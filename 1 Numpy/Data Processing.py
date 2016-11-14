51d42a960cd31811a94bd6dd812bb392021d93b0
Bikash <bikash11roy@gmail.com>
2016-11-15 00:58:38 +0600

    data processing

--- 1 Numpy/Data Processing.py
+++ 1 Numpy/Data Processing.py
@@ -1,10 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 arr = np.arange(-5, 5, .01)
-
-arr = np.arange(5)
-print(arr)
 
 xs, ys = np.meshgrid(arr, arr)
 print(xs)
 print(ys)
+
+z = np.sqrt(xs**2 + ys**2)
+plt.imshow(z, cmap=plt.cm.gray)
+plt.colorbar()
+plt.title("Image plot of $\sqrt{x^2 + y^2}$ for a grid of values")
