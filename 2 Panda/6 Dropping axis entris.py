ae44717b26790b37168a38eca5b36963cdab954a
Bikash <bikash11roy@gmail.com>
2016-11-16 19:39:20 +0600

    Drop axis entrities

--- 2 Panda/6 Dropping axis entris.py
+++ 2 Panda/6 Dropping axis entris.py
@@ -9,3 +9,18 @@ print(new_obj)
 
 new_obj = obj.drop(['c', 'd'])
 print(new_obj)
+
+data = DataFrame(np.arange(16).reshape((4, 4)),
+                 index=['Ohio', 'Colorado', 'Utah', 'New York'],
+                 columns=['one', 'two', 'three', 'four'])
+print(data)
+
+
+data1 = data.drop(['Colorado', 'Ohio'])
+print(data1)
+
+data2 = data.drop('two', axis=1)
+print(data2)
+
+data3 = data.drop(['two', 'three'], axis=1)
+print(data3)
