d555e8c6a43efc1e0787c121625270529a89c6d7
Bikash <bikash11roy@gmail.com>
2016-11-28 03:24:50 +0600

    Histogram and scattering

--- 6 Plotting and Visualization/3 Histogram.py
+++ 6 Plotting and Visualization/3 Histogram.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+from numpy.matlib import randn
 
 # Creating a plot figure object
 fig = plt.figure()
 
 # let's add a subplot
-ax1 = fig.add_subplot(2,3,1)
+ax1 = fig.add_subplot(2,2,1)
 # Here we create a 2x2 space, and slecting the first block
 ax2 = fig.add_subplot(2, 2, 2)
 plt.plot([1.5, 3.5, -2, 1.6])
@@ -16,7 +16,12 @@ ax3 = fig.add_subplot(2,2,3)
 # adding histogram
 mu, sigma = 100, 15
 x = mu + sigma*np.random.randn(10000)
-n, bins, patches = plt.hist(x, 50, normed=1, facecolor='green', alpha=0.75)
+n, bins, patches = ax1.hist(x, 50, normed=1, facecolor='green', alpha=0.75)
+
+
+# Creating a Scatter
+ax2.scatter(np.arange(30), np.arange(30) + randn(30))
+
 
 # finally showing the plot
 plt.show(fig)
