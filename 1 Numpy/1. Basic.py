0f495c7dea0accb025a4025c449ab32be60ad157
Bikash <bikash11roy@gmail.com>
2017-05-26 02:46:30 +0600

    ndarray with calculated dimension

--- 1 Numpy/1. Basic.py
+++ 1 Numpy/1. Basic.py
@@ -4,6 +4,11 @@ import numpy as np
 a = np.arange(15).reshape(3, 5)
 print(a)
 
+a = np.arange(30)
+a.shape = 2,-1,3  # -1 means "whatever is needed"
+print('New shape', a.shape)
+
+
 print(a.shape, a.ndim, a.dtype, a.itemsize, a.size, type(a))
 # (3, 5) 2 int32 4 15 <class 'numpy.ndarray'>
 
