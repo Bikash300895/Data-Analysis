88ceb89cd20b8a869cf770a9ec58d9b3475ff8ec
Bikash <bikash11roy@gmail.com>
2017-05-25 23:22:24 +0600

    Reformating structure and removing unused files

--- 1 Numpy/Element-wise array function.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-arr = np.arange(10)
-print(arr)
-
-print(np.sqrt(arr))
-print(np.exp(arr))
-
-
--- 1 Numpy/Unique and set logic.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Will', 'Joe', 'Joe'])
-
-print(np.unique(names))
-
-print(sorted(set(names)))
