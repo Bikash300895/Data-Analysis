f9ac4d37a8d26c7e60d3a3872c0e5239a38ca23b
Bikash <bikash11roy@gmail.com>
2017-05-25 23:37:02 +0600

    initializing np array

--- 1 Numpy/1. Basic.py
+++ 1 Numpy/1. Basic.py
@@ -7,3 +7,26 @@ print(a)
 print(a.shape, a.ndim, a.dtype, a.itemsize, a.size, type(a))
 # (3, 5) 2 int32 4 15 <class 'numpy.ndarray'>
 
+b = np.array([(1.5,2,3), (4,5,6)])
+print(b)
+
+
+# Working with complex data type
+c = np.array( [ [1,2], [3,4] ], dtype=complex )
+print(c)
+print(c[0][0])
+
+# initializing with values
+print('zeroes',np.zeros((2,2)))
+print(np.ones((2,2), dtype=np.int16))
+print('empty', np.empty((2,2)))
+
+# arrange
+d = np.arange(10,30,5)
+print('arrange',d)
+
+e = np.arange(0, 2, .3)
+print(e)
+
+
+
